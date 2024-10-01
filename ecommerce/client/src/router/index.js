41b5ad7388ea02e583c9b2af6d4fdfcfae428df1
Marcos <markitosanches@gmail.com>
2024-10-01 16:55:40 -0400

    Seance 5

--- ecommerce/client/src/router/index.js
+++ ecommerce/client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import ProductView from '@/views/ProductView.vue'
 
 const routes = [
   {
@@ -9,12 +9,9 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: AboutView
+    path: '/product/:id',
+    name: 'product',
+    component: ProductView
   }
 ]
 
